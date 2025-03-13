55e4c8ea3e1c0c1e850acbb2eea51d588d681b03
GOOMBANGEE <goom@goomba.org>
2025-03-13 13:55:20 +0900

    refactor: extract setting-board-button component

---
+++ src/board/component/setting-board-button.tsx
@@ -0,0 +1,47 @@
+import { useBoardStore } from "../board.store.ts";
+import { Board } from "../board.type.ts";
+
+export default function SettingBoardButton(props: Readonly<Board>) {
+  const { boardState, setBoardState } = useBoardStore();
+
+  const handleClick = () => {
+    setBoardState({
+      id: props.id,
+      title: props.title,
+      icon: props.icon,
+      settingModal: true,
+    });
+  };
+
+  return (
+    <>
+      {props.id === boardState.id && boardState.hover ? (
+        <button onClick={handleClick}>
+          <svg
+            width="32px"
+            height="32px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                d="M18 12H18.01M12 12H12.01M6 12H6.01M13 12C13 12.5523 12.5523 13 12 13C11.4477 13 11 12.5523 11 12C11 11.4477 11.4477 11 12 11C12.5523 11 13 11.4477 13 12ZM19 12C19 12.5523 18.5523 13 18 13C17.4477 13 17 12.5523 17 12C17 11.4477 17.4477 11 18 11C18.5523 11 19 11.4477 19 12ZM7 12C7 12.5523 6.55228 13 6 13C5.44772 13 5 12.5523 5 12C5 11.4477 5.44772 11 6 11C6.55228 11 7 11.4477 7 12Z"
+                stroke="#ffffff"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>
+            </g>
+          </svg>
+        </button>
+      ) : null}
+    </>
+  );
+}
